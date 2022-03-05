f445ab961ad59f9888a14524c5391c78a35f4640
nickus1028 <bentivolio@gmail.com>
2022-03-05 15:00:22 -0500

    Tables defined

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,39 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allownNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allownull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
